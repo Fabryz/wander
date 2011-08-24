5d34698dd920fec7828045f1dd3d6794e91c11f8
Fabrizio Codello <fabryz@gmail.com>
2011-08-24 21:27:23 +0200

    moved inside draw and hasMoved functions

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,6 +1,6 @@
 var Player = function() {
 	this.id = -1;
-	this.nick = "";
+	this.nick = "Unnamed";
 	this.x = 0;
 	this.y = 0;
 	this.vX = 0;
@@ -11,13 +11,14 @@ var Player = function() {
 	this.lastMove = Date.now();
 	this.hp = 100;
 	this.inventory = [];
+	this.ping = 0;
 	
 	this.avatar = new Image();
 	this.avatar.src = './img/player.png';
 	//this.width = serverConfig.tileWidth; FIXME or hardcode?
 	//this.height = serverConfig.tileHeight;
-	this.width = 32;
-	this.height = 32;
+	this.width = 48;
+	this.height = 48;
 	this.halfWidth = this.width/2;
 	this.halfHeight = this.height/2; 		
 	
@@ -35,5 +36,22 @@ var Player = function() {
 
 Player.prototype.toString = function() {
 	//return this.nick +' id: '+ this.id +' x: '+ this.x +' ('+ (this.x+this.width) +') y: '+ this.y +' ('+ (this.y + this.height)+') '+ this.vX +' '+ this.vY;
-	return this.nick +' '+ this.x +':'+ this.y +' ('+ Math.floor(this.x / 32) +':'+ Math.floor(this.y / 32) +')';
+	return this.id +' '+ this.nick +' '+ this.x +':'+ this.y +' ('+ Math.floor(this.x / 48) +':'+ Math.floor(this.y / 48) +') '+ this.ping +'ms';
+};
+
+Player.prototype.draw = function(game) {
+	var coords = game.world.mapToVp(this.x, this.y);
+	
+	if (game.vp.isInside(coords.x, coords.y)) { //TODO: add outer render range, TILE_SIZE * X on if
+		game.ctx.drawImage(this.avatar, coords.x, coords.y, this.width, this.height);
+		game.ctx.fillText(this.nick +' - '+ this.moved, coords.x + this.halfWidth, coords.y - 10);
+	} else {
+		//game.debug(this.id +' is moving out of viewport'); //don't render moving stuff out of viewport
+	}
+};
+
+Player.prototype.hasMoved = function() {
+	this.moved = (this.moveLeft || this.moveRight || this.moveUp || this.moveDown);
+		
+	return this.moved;
 };
