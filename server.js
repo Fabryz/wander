5363c4f5f4ccf1b1324042f3f120e94d2ed7707a
Fabrizio Codello <fabryz@gmail.com>
2011-08-24 21:44:07 +0200

    tiles are now 48x48, added mem usage, fixed player quit bug, using client.id session id

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-/*
-* TODO:
-*		player spawning
-*		collisions
-*		tune up server messages/latency
-*		server authority
-*/
-
 var http = require('http'),
     sys  = require('sys'),
     fs   = require('fs'),
@@ -24,21 +16,24 @@ var serverInfo = {
 	port: 8080,
 	url: "www.serverURL.com",
 	admin: "Fabryz",
-	email: "admin@server.com", //TODO: obfuscate
+	email: "admin@server.com", //TODO: obfuscate?
 	startedAt: Date.now()
 };
 
+//TODO Array with multiple spawns
 var serverConfig = {
-	maxPlayers: 16,
+	maxPlayers: 32,
 	speed: 16,
-	spawnX: 128,
-	spawnY: 128,
+	spawnX: 3,	
+	spawnY: 3,
 	tileMapWidth: 30,
 	tileMapHeight: 30,
-	tileWidth: 32,
-	tileHeight: 32
+	tileWidth: 48,
+	tileHeight: 48,
 };
-    
+serverConfig.pixelMapWidth = serverConfig.tileMapWidth * serverConfig.tileWidth;
+serverConfig.pixelMapHeight = serverConfig.tileMapHeight * serverConfig.tileHeight;
+
 function playerList() {
 	var list = '',
 		i = 0;
@@ -58,18 +53,16 @@ function dateString(date) {
         return (n<10? '0'+n : n);
     }
     
-    return pad(date.getUTCHours())+'h'+ pad(date.getUTCMinutes())+'m'+ pad(date.getUTCSeconds() +'s');
+    return (pad(date.getUTCHours())+'h'+ pad(date.getUTCMinutes())+'m'+ pad(date.getUTCSeconds()) +'s');
 }
 
-function sendStatus(res) {
+function sendServerStatus(res) { //TODO make this an HTML page
 	var memUsed,
-		uptime;
+		uptime,
+		version = JSON.parse(fs.readFileSync(__dirname +'/package.json', 'utf8')).version;
 
-	memUsed = process.memoryUsage();				
-	memUsed = Math.floor((memUsed.heapTotal / 1000000) * 100) / 100;
-	console.log(memUsed +'MB used.');
-
-	res.writeHead(200, { 'Content-Type' : 'text/plain' });				
+	res.writeHead(200, { 'Content-Type' : 'text/plain' });		
+	res.write('Wander v'+ version +'\n\n');		
 	res.write('Server info:\n');
 	res.write('\t'+ serverInfo.name +'\n\t' + serverInfo.ip +':'+ serverInfo.port +'\n');
 	res.write('\t'+ serverInfo.url +'\n\n\t'+ serverInfo.desc +'\n\n');
@@ -81,8 +74,14 @@ function sendStatus(res) {
 		res.write('Player list:\n'+ playerList() +'\n');
 	} 
 
-	uptime = new Date(Date.now() - serverInfo.startedAt); //TODO: use process.uptime?
-	res.end('Uptime: '+ dateString(uptime) +'\n');
+	uptime = new Date(Date.now() - serverInfo.startedAt); //TODO: use process.uptime? need Node >v0.5
+	res.write('Uptime: '+ dateString(uptime) +'\n');
+	
+	memUsed = process.memoryUsage();				
+	memUsed = Math.floor((memUsed.heapTotal / 1000000) * 100) / 100; //bytes to MB, round decimal
+	res.end('Memory: '+ memUsed +'MB used.\n');
+	
+	console.log('Memory: '+ memUsed +'MB used, Uptime: '+ dateString(uptime) +', total players: '+ totPlayers);
 }
 
 /*
@@ -96,80 +95,69 @@ var app = express.createServer(
 	);
 
 app.get('/status', function(req, res){
-    sendStatus(res);
+    sendServerStatus(res);
 });
 
 app.listen(serverInfo.port);
-console.log('Server started on Node '+ process.version +', platform '+ process.platform +'.');
+
+serverInfo.ip = app.address().address;
+serverInfo.port = app.address().port; //actual server port
+
+console.log('Server started at '+ serverInfo.ip +':'+ serverInfo.port +' with Node '+ process.version +', platform '+ process.platform +'.');
 
 /*
 * Socket stuff
 */
 	
 //player template test
-function Player(id, nick) {
+function Player(id) {
 	this.id = id;
-	this.nick = nick;
-	this.x = serverConfig.spawnX;
-	this.y = serverConfig.spawnY;
-	
-	this.playing = false;
+	this.nick = 'Guest'+ id;
+	this.x = serverConfig.spawnX * serverConfig.tileWidth;
+	this.y = serverConfig.spawnY * serverConfig.tileHeight;
+	this.ping = 0;
 }
 
 function sendServerInfo(client) {
-	client.emit('info', { msg: '# Welcome to "'+ serverInfo.name +'" server! ('+serverInfo.ip +':'+ serverInfo.port +')' });
+	client.emit('info', { msg: '# Welcome to "'+ serverInfo.name +'" server! ('+ serverInfo.ip +':'+ serverInfo.port +')' });
 	client.emit('info', { msg: '# There are now '+ totPlayers +' players online.' });
 }
 
 function sendServerConfig(client) {
-	client.emit('config', { config: json(serverConfig) });
-	console.log('Sent server config');
+	client.emit('config', { config: serverConfig });
+	console.log('+ Sent server config to '+ client.id);
 }
 	
-function newPlayer(id, client) {
+function newPlayer(client) {
 	sendServerInfo(client);
 	sendServerConfig(client);
 	
-	p = new Player(id, 'Guest'+id);	
+	p = new Player(client.id);	
 	players.push(p);
-
-	client.emit('join', { player: json(p) });
-	io.sockets.emit('newPlayer', { player: json(p) });
 	
-	console.log('+ newPlayer: '+ json(p));
-	return p;
+	client.set('id', client.id, function () {
+    	client.emit('join', { player: p });
+    });	
+	client.broadcast.emit('newPlayer', { player: p }); //send event to all BUT not the current client
+	
+	console.log('+ New player: '+ p.nick);
 }
 
 function sendPlayerList(client) {
-	console.log('Requested player list');
-	/*if (players.length > 0) {
-		players.forEach(function(pla) {
-			if (pla.id != playerid) {
-				client.emit('playersList', { x: pla.x, y: pla.y, id: pla.id, nick: pla.nick });
-			}
-		});
-	} else {
-		client.emit('playersList', msg: 'You are alone in the server' });
-	}*/
-
-	
-	client.emit('playersList', { list: json(players) });
-	console.log('Sent player list: '+ json(players) );
+	client.emit('list', { list: players });
+	console.log('* Sent player list to '+ client.id);
 }
 
 function checkBounds(player) {
-	var pixelMapWidth = serverConfig.tileMapWidth * serverConfig.tileWidth,
-		pixelMapHeight = serverConfig.tileMapHeight * serverConfig.tileHeight;
-
-	if (player.x + serverConfig.tileWidth > pixelMapWidth) { //TODO: player is as large as a map tile?
-		player.x = pixelMapWidth - serverConfig.tileWidth;
+	if (player.x + serverConfig.tileWidth > serverConfig.pixelMapWidth) { //TODO: player is as large as a map tile?
+		player.x = serverConfig.pixelMapWidth - serverConfig.tileWidth;
 	}
 	if (player.x < 0) {
 		player.x = 0;
 	}
 	
-	if (player.y + serverConfig.tileHeight > pixelMapHeight) {
-		player.y = pixelMapHeight - serverConfig.tileHeight;
+	if (player.y + serverConfig.tileHeight > serverConfig.pixelMapHeight) {
+		player.y = serverConfig.pixelMapHeight - serverConfig.tileHeight;
 	}
 	if (player.y < 0) {
 		player.y = 0;
@@ -178,12 +166,12 @@ function checkBounds(player) {
 	return player;
 }
 
-function sendGameData(player, data) {
+function sendGameData(client, data) {
 	/* TODO: Do bounds and anticheat checks*/
 	/* Elaborate next position, send confirmed position to client */
 
 	players.forEach(function(p) {
-		if (p.id == player.id) {
+		if (p.id == client.id) {
 			if (data.dir == 'l') {
 				p.x += -serverConfig.speed;
 			}
@@ -197,6 +185,7 @@ function sendGameData(player, data) {
 				p.y += serverConfig.speed;
 			}
 			p = checkBounds(p);
+			
 			io.sockets.emit('play', { id: p.id, x: p.x, y: p.y });
 		}
 	});	
@@ -205,8 +194,6 @@ function sendGameData(player, data) {
 var io = require('socket.io').listen(app),
 	players = [],
 	totPlayers = 0,
-	currentId = -1,
-	currentPid = -1,
 	json = JSON.stringify;
 	
 io.configure(function(){ 
@@ -222,18 +209,16 @@ io.configure(function(){
 }); 
 
 io.sockets.on('connection', function(client) {	
-	totPlayers++;
-	currentId++;
-	var player = newPlayer(currentId, client);
-	
-	sendPlayerList(client);
-	
 	var address = client.handshake.address;
-	console.log('* New connection from ' + address.address + ':' + address.port);	
-	console.log('* NEW ' +json(player));	
-	console.log('* Player connected, total players: '+ totPlayers);
+	console.log('+ New connection from ' + address.address);
 		
-	client.on('message', function(mess) {		
+	newPlayer(client);	
+	sendPlayerList(client);	
+
+	totPlayers++;
+	console.log('+ Player connected, total players: '+ totPlayers);
+		
+	client.on('message', function(mess) { //forever alone
 		try {
             var data = JSON.parse(mess);
         } catch (err) {
@@ -245,61 +230,50 @@ io.sockets.on('connection', function(client) {
 		console.log('Arrived: '+ mess);
 					
 		switch (data.type) {
-				
 			default:
 				console.log('[Error] Unknown message type: '+ mess);
 			break;
 		}
 
 	});
-	
-	client.on('setNick', function(data) {
-		//var data = JSON.parse(mess);
+
+	client.on('setNick', function(data) {	
+		//TODO: check doublenick FIXME check stuff directly on nickchange and remove nickres
 		
-		//todo: must grab and update also on players array
-		//players[player.id].nick = data.nick;
 		players.forEach(function(p) {
-			if (p.id == player.id) {
+			if (p.id == data.id) {
 				p.nick = data.nick;
 			}
 		});
-		io.sockets.emit('nickChange', { id: player.id, nick: data.nick });
-		console.log('Nick set to '+ data.nick);
-		
-		//todo: check doublenick
-		client.emit('nickRes', { res: 'ok' });	
+
+		io.sockets.emit('nickChange', { id: data.id, nick: data.nick });
+		client.emit('nickRes', { res: true });	
+		console.log('* '+ data.id +' changed his nick to '+ data.nick);
 	});
 	
-	client.on('playersList', function() {
-		sendPlayerList(client);
+	client.on('play', function(data) {	
+		sendGameData(client, data);
 	});
 	
-	client.on('play', function(data) {
-		//var data = JSON.parse(mess);
-		
-		sendGameData(player, data);
-	});
-	
-	client.on('ping', function(data) {
-		//var data = JSON.parse(mess);
-		
+	client.on('ping', function(data) {	
+		//console.log('Ping from '+ data.id);	
 		client.emit('pong', { time: Date.now() });
 	});
 
 	client.on('disconnect', function() {
+		var quitter,
+			i = 0;
+		
 		players.forEach(function(p) {
-			if (p.id == player.id) {
-				players.splice(0, 1);
+			if (p.id == client.id) {
+				quitter = new Player(p.id);
+				quitter = p;
+				players.splice(i, 1);
 			}
+			i++;
 		});
-		io.sockets.emit('quit', { id: player.id });
-		console.log('sent player quit: '+ player.id );
-		
-		
+		client.broadcast.emit('quit', { id: client.id });
 		totPlayers--;
-		console.log('- Player '+ player.nick +' disconnected, total players: '+ totPlayers);
-		player = {};
-		
-		console.log('Current player list: ' +json(players));
+		console.log('- Player '+ quitter.nick +' ('+ client.id +') disconnected, total players: '+ totPlayers);
 	});
 });
