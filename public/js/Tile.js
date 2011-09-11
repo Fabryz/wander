3050275822be6e9970a9563fe0b879d0ab6c99b9
Fabrizio Codello <fabryz@gmail.com>
2011-09-11 23:02:21 +0200

    now shared between client/server

--- public/js/Tile.js
+++ public/js/Tile.js
@@ -1,8 +1,16 @@
-var Tile = function(id, name, src, block) {
+(function(exports) {
+
+var Tile = function(id, name, src, walkable) {
 	this.id = id;
 	this.name = name;
 	this.src = src;
-	this.block = block;
+	this.walkable = walkable;
+};
+
+Tile.prototype.preLoad = function () {  //preload images client side
 	this.image = new Image();
 	this.image.src = this.src;
 };
+
+exports.Tile = Tile;
+})(typeof global === "undefined" ? window : exports);
