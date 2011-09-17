e0b95b9b102a595ee6707e633471fa7699b8c52d
Fabrizio Codello <fabryz@gmail.com>
2011-09-17 13:28:13 +0200

    each tile can have extra fields, testing teleport

--- public/js/Tile.js
+++ public/js/Tile.js
@@ -1,10 +1,11 @@
 (function(exports) {
 
-var Tile = function(id, name, src, walkable) {
+var Tile = function(id, name, src, walkable, extra) {
 	this.id = id;
 	this.name = name;
 	this.src = src;
 	this.walkable = walkable;
+	this.extra = extra || undefined;
 };
 
 Tile.prototype.preLoad = function () {  //preload images client side
