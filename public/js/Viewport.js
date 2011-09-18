8afe6fbb6aae7a37a9fb5ad998d112ce3478ef63
Fabrizio Codello <fabryz@gmail.com>
2011-09-18 22:56:30 +0200

    adding safety border range on isInside check

--- public/js/Viewport.js
+++ public/js/Viewport.js
@@ -6,14 +6,19 @@ var Viewport = function(game, width, height) {
 	this.y = 0;
 };
 
-Viewport.prototype.centerOn = function(obj) {
-	//this.game.debug('Centering on: '+ obj.x +':'+ obj.y);
-	this.x = obj.x - (this.width / 2);
-	this.y = obj.y - (this.height / 2);
+Viewport.prototype.centerOn = function(x, y) {
+	//this.game.debug('Centering on: '+ x +':'+ y);
+	this.x = x - (this.width / 2);
+	this.y = y - (this.height / 2);
 };
 
-Viewport.prototype.isInside = function(x, y) { //FIXME map2stuff on canvaswidth 
-	if (((x >= 0) && (x < this.width)) && ((y >= 0) && (y < this.height))) {
+//If is inside viewport + tilesize * range (safety border)
+Viewport.prototype.isInside = function(x, y) {
+	var rangeTileWidth = this.game.serverConfig.tileWidth * 1,
+		rangeTileHeight = this.game.serverConfig.tileHeight * 1;
+
+	//FIXME if (((x >= 0) && (x < this.width)) && ((y >= 0) && (y < this.height))) {
+	if (((x >= -rangeTileWidth) && (x < this.width + rangeTileWidth)) && ((y >= -rangeTileHeight) && (y < this.height + rangeTileHeight))) {
 		return true;
 	}
 	
