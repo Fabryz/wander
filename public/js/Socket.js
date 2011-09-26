61349f07d33d75ecfb81c6d644a127c5406b4e6f
Fabrizio Codello <fabryz@gmail.com>
2011-09-27 00:35:06 +0200

    added socket namespaces

--- public/js/Socket.js
+++ public/js/Socket.js
@@ -1,5 +1,7 @@
-var Socket = function(game) {	
-	this.socket = new io.connect('http://' + window.location.hostname);
+var Socket = function(game, namespace) {
+	this.namespace = namespace || 'game';
+	this.url = window.location.hostname +'/'+ this.namespace;
+	this.socket = new io.connect('http://'+ this.url);
 	
 	if (game) {
 		game.debug('Connecting...');
