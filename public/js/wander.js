26c20f5a22796dac561959e979fd6be7e9088fd4
Fabrizio Codello <fabryz@gmail.com>
2011-10-08 12:12:22 +0200

    testing requestAnimFrame

--- public/js/wander.js
+++ public/js/wander.js
@@ -498,6 +498,52 @@ $(document).ready(function() {
 		}
 	}
 	
+	/* testing requestAnimFrame */
+	
+	window.requestAnimFrame = (function(){
+		return  window.requestAnimationFrame       || 
+				window.webkitRequestAnimationFrame || 
+				window.mozRequestAnimationFrame    || 
+				window.oRequestAnimationFrame      || 
+				window.msRequestAnimationFrame     || 
+				function(callback, element){
+					window.setTimeout(callback, 1000 / game.desiredFPS);
+				};
+	})();
+	
+	window.requestTimeout = function(fn, delay) {
+		if( !window.requestAnimationFrame      	&& 
+			!window.webkitRequestAnimationFrame && 
+			!(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
+			!window.oRequestAnimationFrame      && 
+			!window.msRequestAnimationFrame)
+				return window.setTimeout(fn, delay);
+			
+		var start = new Date().getTime(),
+			handle = new Object();
+		
+		function loop(){
+			var current = new Date().getTime(),
+				delta = current - start;
+			
+			delta >= delay ? fn.call() : handle.value = requestAnimFrame(loop);
+		};
+	
+		handle.value = requestAnimFrame(loop);
+		return handle;
+	};
+
+	window.clearRequestTimeout = function(handle) {
+		window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
+		window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value)	:
+		window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
+		window.oCancelRequestAnimationFrame	? window.oCancelRequestAnimationFrame(handle.value) :
+		window.msCancelRequestAnimationFrame ? msCancelRequestAnimationFrame(handle.value) :
+		clearTimeout(handle);
+	};
+	
+	/* /testing requestAnimFrame */
+	
 	function gameLoop() {
 		game.world.clearAll();
 		
@@ -516,7 +562,8 @@ $(document).ready(function() {
 			game.fps.count++;
 			//game.tick_count++;
 
-			setTimeout(gameLoop, 30); //test! - 1000/desired_fps
+			//setTimeout(gameLoop, 30); //test! - 1000/desired_fps
+			requestTimeout(gameLoop, 1000 / game.desiredFPS);
 		}
 	}
 
